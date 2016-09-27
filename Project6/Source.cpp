2ac34191c3d081079b42ad3a5428cf82a44f0527
griftmens <joaquin_oa@hotmail.com>
2016-09-27 10:18:23 -0500

    Ingrese siionoo!!!

---
+++ Project6/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+
+using namespace System;
+using namespace std;
+
+int main()
+{
+
+	cout << "SIIIONOOOO!!!!!!"
+
+	_getch();
+	return 0;
+}
